3c332d79721dc558e1ca1c9ca081dc41aae83cec
huuvuno1 <nguyenhuuvuno1@gmail.com>
2022-01-19 21:43:52 +0700

    deploy

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 require('dotenv').config()
 const express =require('express');
-const fs = require('fs');
+// const fs = require('fs');
 const routerConfig = require('./routers/index.router');
 const cookieParser = require('cookie-parser');
 const socketConfig = require('./socket.io/index.socket');
 const app = express()
 
-const privateKey  = fs.readFileSync('./ssl/private_key.pem', 'utf8');
-const certificate = fs.readFileSync('./ssl/certificate.pem', 'utf8');
-const credentials = {key: privateKey, cert: certificate};
+// const privateKey  = fs.readFileSync('./ssl/private_key.pem', 'utf8');
+// // const certificate = fs.readFileSync('./ssl/certificate.pem', 'utf8');
+// const credentials = {key: privateKey, cert: certificate};
 
 const server = require("http").Server(app);
-const serverSsl = require("https").Server(credentials, app);
+// const serverSsl = require("https").Server(credentials, app);
 const io = require("socket.io")(server);
 
 app.set('view engine', 'ejs');
@@ -26,6 +26,6 @@ server.listen(3000, () => {
     console.log('server is running on port 3000')
 })
 
-serverSsl.listen(3443, () => {
-    console.log('server is running on port 443')
-})
+// serverSsl.listen(3443, () => {
+//     console.log('server is running on port 443')
+// })
