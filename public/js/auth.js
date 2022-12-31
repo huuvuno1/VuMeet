c76fcc90e76220228dd601e6cbf8aeb0532da412
huuvuno1 <nguyenhuuvuno1@gmail.com>
2022-12-31 23:10:58 +0700

    change client id gg

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,43 +1,44 @@
 function handleCredentialResponse(response) {
-    // console.log("Encoded JWT ID token: " + response.credential);
-    
-    fetch('/api/login', {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            token: response.credential
-        })
+  // console.log("Encoded JWT ID token: " + response.credential);
+
+  fetch("/api/login", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      token: response.credential,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      location.reload();
     })
-        .then(response => response.json())
-        .then(data => {
-            location.reload()
-        })
-        .catch(err => console.log(err))
+    .catch((err) => console.log(err));
 }
 window.onload = function () {
-    google.accounts.id.initialize({
-    client_id: "60630415904-95rejsmmjkgb7ffgsvc0mme7u0ar1ujt.apps.googleusercontent.com",
-    callback: handleCredentialResponse
-    });
-    google.accounts.id.renderButton(
+  google.accounts.id.initialize({
+    client_id:
+      "190359929605-db8140sku5ogdcl8mfkjjueubo49st8g.apps.googleusercontent.com",
+    callback: handleCredentialResponse,
+  });
+  google.accounts.id.renderButton(
     document.getElementById("btnLoginWithGG"),
-    { theme: "outline", size: "large" }  // customization attributes
-    );
-    google.accounts.id.prompt(); // also display the One Tap dialog
-}
+    { theme: "outline", size: "large" } // customization attributes
+  );
+  google.accounts.id.prompt(); // also display the One Tap dialog
+};
 
 function logout() {
-    location.href = '/logout'
+  location.href = "/logout";
 }
 
 function createZoom() {
-    fetch('/api/zooms', {method: 'POST'})
-        .then(response => response.json())
-        .then(data => location.href = `/${data.zoom_id}`)
-        .catch(err => {
-            console.log(err)
-            alert('Login đã bro ơi, bên trên góc phải màn hình á')
-        })
+  fetch("/api/zooms", { method: "POST" })
+    .then((response) => response.json())
+    .then((data) => (location.href = `/${data.zoom_id}`))
+    .catch((err) => {
+      console.log(err);
+      alert("Login đã bro ơi, bên trên góc phải màn hình á");
+    });
 }
